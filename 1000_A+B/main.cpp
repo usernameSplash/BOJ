6b0690f4ded3491d678883ecd221eef1ef3578c0
김경민 <mini@gimgyeongmins-MacBook-Pro.local>
2020-12-03 00:13:23 +0900

    1000 - A + B

---
+++ 1000_A+B/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void cppIOInitialize()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main()
+{
+    cppIOInitialize();
+    int a, b;
+    cin >> a >> b;
+    cout << (a + b);
+}
