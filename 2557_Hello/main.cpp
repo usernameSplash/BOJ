34741d1cfbe0df434671904facafacfee849a631
김경민 <mini@gimgyeongmins-MacBook-Pro.local>
2020-12-03 00:10:27 +0900

    2557 - Hello World

---
+++ 2557_Hello/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void cppIOInitialize()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main()
+{
+    cppIOInitialize();
+    cout << "Hello World!";
+}
