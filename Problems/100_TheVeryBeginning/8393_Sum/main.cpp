e09c9f28d3c744e5ea0804a833079169a594f32f
김경민 <kkfenix3085@naver.com>
2020-12-06 00:49:49 +0900

    8393 : Sum

--- Problems/100_TheVeryBeginning/8393_Sum/main.cpp
+++ Problems/100_TheVeryBeginning/8393_Sum/main.cpp
@@ -13,20 +13,7 @@ void cppIOInitialize()
 int main()
 {
     cppIOInitialize();
-    int m, d, delta_m, delta_d, result;
-    cin >> m >> d;
-
-    delta_m = m - 1;
-    delta_d = d - 1;
-
-    const array<int, 12> days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    const array<string, 7> dates = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
-
-    for (int i = 0; i < delta_m; i++)
-    {
-        delta_d += days[i];
-    }
-    result = delta_d % 7;
-
-    cout << dates[result] << endl;
+    int n;
+    cin >> n;
+    cout << (n * (n + 1)) / 2 << "\n";
 }
