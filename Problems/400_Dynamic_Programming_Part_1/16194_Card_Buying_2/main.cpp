d6ecd8c91c5f8f807380d0ee119d8eec64e07d53
김경민 <kkfenix3085@naver.com>
2020-12-08 00:15:59 +0900

    16194 - Card Buying 2

---
+++ Problems/400_Dynamic_Programming_Part_1/16194_Card_Buying_2/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void cppIOInitialize()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int CardBuying(vector<int> price)
+{
+    int length = price.size();
+
+    int dp[1001] = {0};
+
+    for (int i = 1; i <= length - 1; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (dp[i] == 0) // 이 부분이 카드 구매하기 1과 다르다.
+                dp[i] = dp[i - j] + price[j];
+            else
+                dp[i] = min(dp[i], dp[i - j] + price[j]);
+        }
+    }
+
+    return dp[length - 1];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> price;
+    price.push_back(0); //index 0 위치에 더미데이터를 넣어주었다.
+
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        price.push_back(temp);
+    }
+
+    cout << CardBuying(price);
+    cout << "\n";
+
+    return 0;
+}
