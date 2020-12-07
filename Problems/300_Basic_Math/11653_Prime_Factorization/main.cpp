69a4856c61c9b7eb009850b74b24dc03dda985e4
김경민 <kkfenix3085@naver.com>
2020-12-07 22:13:41 +0900

    11653 - Prime Factorization

---
+++ Problems/300_Basic_Math/11653_Prime_Factorization/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void cppIOInitialize()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+vector<int> getPrimeFactors(int n)
+{
+    vector<int> result;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            result.push_back(i);
+            n /= i;
+        }
+    }
+
+    if (n > 1)
+    {
+        result.push_back(n);
+    }
+
+    return result;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v = getPrimeFactors(n);
+
+    for (auto num : v)
+    {
+        cout << num << "\n";
+    }
+
+    return 0;
+}
