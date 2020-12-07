de5492303ca6da3a24a666408e5a9fa309679b03
김경민 <kkfenix3085@naver.com>
2020-12-07 23:38:43 +0900

    11726 2*N Tiles 11727 2*N Tiles(2)

---
+++ Problems/400_Dynamic_Programming_Part_1/11726_2xN_Tiles/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void cppIOInitialize()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int Tiling2xN(int n)
+{
+    int dp[n + 1];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+    }
+
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << Tiling2xN(n);
+
+    cout << "\n";
+
+    return 0;
+}
