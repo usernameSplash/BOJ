25d2d340468165b9cc8af2286e3dd2da6291fd33
김경민 <kkfenix3085@naver.com>
2020-12-11 03:49:37 +0900

    15654 - N and M (5)

---
+++ Problems/510_Brute_Force_N_and_M/15654_N_and_M_5/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+void cppIOInitialize()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int a[10] = {0};      // data
+bool v[10] = {false}; // visited
+
+void Permutation(vector<int> &data, int index, int n, int m)
+{
+    if (index == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!v[i])
+        {
+            v[i] = true;
+            a[index] = data[i];
+            Permutation(data, index + 1, n, m);
+            v[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> data(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> data[i];
+    }
+    sort(data.begin(), data.end());
+
+    Permutation(data, 0, n, m);
+
+    cout << "\n";
+
+    return 0;
+}
