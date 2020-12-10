7a7acc24488710c04dfbd9a36d9f073027268086
김경민 <kkfenix3085@naver.com>
2020-12-11 03:55:13 +0900

    15656 - N and M (7)

---
+++ Problems/510_Brute_Force_N_and_M/15656_N_and_M_7/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+void cppIOInitialize()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int a[10] = {0};      // data
+bool v[10] = {false}; // visited
+
+void Permutation(vector<int> &data, int index, int n, int m)
+{
+    if (index == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        a[index] = data[i];
+        Permutation(data, index + 1, n, m);
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> data(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> data[i];
+    }
+    sort(data.begin(), data.end());
+
+    Permutation(data, 0, n, m);
+
+    cout << "\n";
+
+    return 0;
+}
