20625c86a0017696a2b7a5917b783b311c5585a0
김경민 <kkfenix3085@naver.com>
2020-12-14 22:11:08 +0900

    14391 - Piece Of Paper

--- Problems/540_Bitmask/14391_Piece_Of_Paper/main.cpp
+++ Problems/540_Bitmask/14391_Piece_Of_Paper/main.cpp
@@ -4,39 +4,36 @@
 
 using namespace std;
 
-
 int main()
 {
     int n, m;
     scanf("%d %d", &n, &m);
-    
+
     int data[n][m];
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j = 0; j < m; j++)
+        for (int j = 0; j < m; j++)
         {
             scanf("%1d", &data[i][j]);
         }
     }
 
-    int s = 0;    // 비트마스킹 변수
+    int s = 0; // 비트마스킹 변수
     int max_val = 1 << (n * m);
 
     int result = 0;
 
-    for(s = 0; s < max_val; s++)
+    for (s = 0; s < max_val; s++)
     {
         int sum = 0;
-
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             int cur = 0;
-            for(int j = 0; j < m; j++)
+            for (int j = 0; j < m; j++)
             {
                 int k = i * m + j;
-
-                if((s & (1 << k)) == 0)
+                if ((s & (1 << k)) == 0)
                 {
                     cur = cur * 10 + data[i][j];
                 }
@@ -47,16 +44,15 @@ int main()
                 }
             }
             sum += cur;
-        } 
+        }
 
-        for(int j = 0; j < m; j++)
+        for (int j = 0; j < m; j++)
         {
             int cur = 0;
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 int k = i * m + j;
-
-                if((s & (1 << k)) > 0)
+                if ((s & (1 << k)) > 0)
                 {
                     cur = cur * 10 + data[i][j];
                 }
@@ -72,5 +68,4 @@ int main()
     }
 
     printf("%d\n", result);
-
 }
